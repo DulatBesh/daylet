1a8d1e7ebead229b54a033b42a5918c162b55e64
Dulat <dulat.sagyntay@bk.ru>
2022-09-22 22:05:16 +0600

    work js

--- js/script.js
+++ js/script.js
@@ -79,23 +79,24 @@
 // alert(`Привет меня зовут ${user}`);
 
 
-console.log('arr' + " - object");
-console.log(4 + "5");
+// console.log('arr' + " - object");
+// console.log(4 + "5");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// ++incr;
-// decr--;
+// // ++incr;
+// // decr--;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2*5 !== 8);
+// console.log(2*5 !== 8);
 
-const isChecked = true,
-      isClose = false;
+// const isChecked = true,
+//       isClose = false;
 
-console.log(isChecked || isClose);
+// console.log(isChecked || isClose);
+
